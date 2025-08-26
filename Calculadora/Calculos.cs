eb64f18bf492aa1e12fe59ff1493eb994aa001a8
mathmenegon <menegon775@gmail.com>
2025-08-25 23:09:23 -0300

    Adicionado o While pra criar o loop no menu, e o ReadKey para que o resultado pudesse ser mantido na tela

--- Calculadora/Calculos.cs
+++ Calculadora/Calculos.cs
@@ -12,13 +12,16 @@ namespace Calculadora
     {
         public void MenuSelecao()
         {
-
+            while (true) 
+            { 
                 Console.Clear();
-                Console.WriteLine("Selecione a operação desejada:");
+                Console.WriteLine("Menu de opções:");
                 Console.WriteLine("1 - Soma");
                 Console.WriteLine("2 - Subtração");
                 Console.WriteLine("3 - Multiplicação");
                 Console.WriteLine("4 - Divisão");
+                Console.WriteLine("5 - Sair");
+                Console.WriteLine("\nDigite o numero da operação desejada:");
 
                 double operacao = double.Parse(Console.ReadLine()!);
 
@@ -32,13 +35,19 @@ namespace Calculadora
                         Multiplicacao(); break;
                     case 4:
                         Divisao(); break;
+                    case 5:
+                        return;
                     default:
-                        MenuSelecao(); break;
+                        Console.WriteLine("Opção inválida! Pressiona Enter para continuar"); break;
+                }
+                Console.WriteLine("\nPressione Enter para Voltar");
+                Console.ReadKey();
             }
         }
 
         static (double v1, double v2) ObterDados()
         {
+            Console.Clear();
             Console.WriteLine("Digite o primeiro valor:");
             double v1 = double.Parse(Console.ReadLine()!);
             Console.WriteLine("Digite o segundo valor:");
